4ffca0caa04ed108421de0980e56852d4ebf82e6
copilotgitpod <155841626+copilotgitpod@users.noreply.github.com>
2024-01-11 22:39:20 +0000

    d

--- api/index.py
+++ api/index.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # You need to install flask-cors first
 import random
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 news = [
     {'title': 'News 1', 'content': 'Content 1'},
     {'title': 'News 2', 'content': 'Content 2'},
     {'title': 'News 3', 'content': 'Content 3'},
-    # Add more news here...
+    # ... Add more news here
 ]
 
 @app.route('/api/news/', methods=['GET'])
@@ -18,5 +20,5 @@ def get_all_news():
 def get_random_news():
     return jsonify(random.choice(news))
 
-if __name__ == '__main__':
+if name == '__main__':
     app.run(debug=True)
